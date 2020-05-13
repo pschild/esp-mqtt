c93c8f7d8d7f198b92fdccdb2f51bd1027ae5caf
pschild <philippe.schild@googlemail.com>
2020-05-13 22:29:14 +0200

    fixes

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,9 @@
 #endif
 
 const char* MQTT_BROKER = "192.168.178.28";
-char* version;
 
 WiFiClient espClient;
-PubSubClient client(espClient);
+PubSubClient mqttClient(espClient);
 
 long lastMsg = 0;
 char msg[50];
@@ -28,6 +27,9 @@ int value = 0;
 
 unsigned long lastUpdateCheck = 0;
 const long UPDATE_INTERVAL = 1 * 60 * 60 * 1000; // 1 hour
+
+unsigned long lastPing = 0;
+const long PING_INTERVAL = 60 * 1000; // 1 min
 
 void connectToWifi() {
   WiFi.mode(WIFI_STA);
@@ -50,22 +52,30 @@ void checkForUpdate(bool forceUpdate) {
   ) {
     WiFiClient client;
     ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
-    ESPhttpUpdate.update(client, "http://192.168.178.28:9042/ota", version);
+    ESPhttpUpdate.update(client, "http://192.168.178.28:9042/ota", VERSION);
     lastUpdateCheck = now;
   }
 }
 
+void sendPing() {
+  unsigned long now = millis();
+  if (now >= lastPing + PING_INTERVAL) {
+    mqttClient.publish("/devices/nodemcu/version", VERSION);
+    lastPing = now;
+  }
+}
+
 void reconnectMqttClient() {
-  while (!client.connected()) {
+  while (!mqttClient.connected()) {
     Serial.print("Reconnecting...");
-    if (!client.connect("NodeMcuClient")) {
+    if (!mqttClient.connect("NodemcuClient")) {
       Serial.print("failed, rc=");
-      Serial.print(client.state());
+      Serial.print(mqttClient.state());
       Serial.println(" retrying in 5 seconds");
       delay(5000);
     }
   }
-  client.subscribe("/foo/bar");
+  mqttClient.subscribe("/foo/bar");
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -73,7 +83,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print(topic);
   Serial.print("] ");
   char msg[length+1];
-  for (int i = 0; i < length; i++) {
+  for (unsigned int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
     msg[i] = (char)payload[i];
   }
@@ -81,28 +91,30 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   msg[length] = '\0';
   Serial.println(msg);
+  if (strcmp(msg, "on") == 0) {
+    digitalWrite(LED_BUILTIN, LOW);
+  } else if (strcmp(msg, "off") == 0) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
 }
 
 void setup() {
-  if (VERSION == "") {
-    version = "unknown";
-  }
-
   Serial.begin(9600);
   connectToWifi();
   checkForUpdate(true);
-  client.setServer(MQTT_BROKER, 1883);
-  client.setCallback(callback);
+  mqttClient.setServer(MQTT_BROKER, 1883);
+  mqttClient.setCallback(callback);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
   checkForUpdate(false);
+  sendPing();
 
-  if (!client.connected()) {
+  if (!mqttClient.connected()) {
     reconnectMqttClient();
   }
-  client.loop();
-
-  client.publish("/devices/NodeMcuClient/version", version);
-  delay(5000);
+  mqttClient.loop();
 }
