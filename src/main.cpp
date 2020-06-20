45938b94e9b540705bad8841e1ef0f4eac7869b9
pschild <philippe.schild@googlemail.com>
2020-06-20 16:14:06 +0200

    use custom lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <ESP8266httpUpdate.h>
-#include <PubSubClient.h>
+#include <Ticker.h>
+#include <MqttHandler.h>
+#include <OTAUpdateHandler.h>
 
 #ifndef WIFI_SSID
   #error "Missing WIFI_SSID"
@@ -16,20 +16,13 @@
   #error "Missing VERSION"
 #endif
 
-const char* MQTT_BROKER = "192.168.178.28";
+void ping();
+void onFooBar(char* payload);
+void onMqttConnected();
 
-WiFiClient espClient;
-PubSubClient mqttClient(espClient);
-
-long lastMsg = 0;
-char msg[50];
-int value = 0;
-
-unsigned long lastUpdateCheck = 0;
-const long UPDATE_INTERVAL = 1 * 60 * 60 * 1000; // 1 hour
-
-unsigned long lastPing = 0;
-const long PING_INTERVAL = 60 * 1000; // 1 min
+MqttHandler mqttHandler("192.168.178.28", String("ESP_") + String(ESP.getChipId()));
+OTAUpdateHandler updateHandler("192.168.178.28:9042", VERSION);
+Ticker pingTimer(ping, 60 * 1000);
 
 void connectToWifi() {
   WiFi.mode(WIFI_STA);
@@ -44,83 +37,38 @@ void connectToWifi() {
   }
 }
 
-void checkForUpdate(bool forceUpdate) {
-  unsigned long now = millis();
-  if (
-    WiFi.status() == WL_CONNECTED
-    && (now >= lastUpdateCheck + UPDATE_INTERVAL || forceUpdate == true)
-  ) {
-    WiFiClient client;
-    ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
-    ESPhttpUpdate.update(client, "http://192.168.178.28:9042/ota", VERSION);
-    lastUpdateCheck = now;
-  }
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  connectToWifi();
+
+  mqttHandler.setOnConnectedCallback(onMqttConnected);
+  mqttHandler.setup();
+
+  pingTimer.start();
 }
 
-void sendPing() {
-  unsigned long now = millis();
-  if (now >= lastPing + PING_INTERVAL) {
-    mqttClient.publish("/devices/nodemcu/version", VERSION);
-    lastPing = now;
-  }
+void loop() {
+  mqttHandler.loop();
+  updateHandler.loop();
+
+  pingTimer.update();
 }
 
-void reconnectMqttClient() {
-  while (!mqttClient.connected()) {
-    Serial.print("Reconnecting...");
-
-    char clientName[20];
-    strcpy(clientName, "ESP_");
-    strcat(clientName, String(ESP.getChipId()).c_str());
-    puts(clientName);
-
-    if (!mqttClient.connect(clientName)) {
-      Serial.print("failed, rc=");
-      Serial.print(mqttClient.state());
-      Serial.println(" retrying in 5 seconds");
-      delay(5000);
-    }
-  }
-  mqttClient.subscribe("/foo/bar");
+void ping() {
+  mqttHandler.publish("/devices/nodemcu/version", VERSION);
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Received message [");
-  Serial.print(topic);
-  Serial.print("] ");
-  char msg[length+1];
-  for (unsigned int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-    msg[i] = (char)payload[i];
-  }
-  Serial.println();
-
-  msg[length] = '\0';
-  Serial.println(msg);
-  if (strcmp(msg, "on") == 0) {
+void onFooBar(char* payload) {
+  if (strcmp(payload, "on") == 0) {
     digitalWrite(LED_BUILTIN, LOW);
-  } else if (strcmp(msg, "off") == 0) {
+  } else if (strcmp(payload, "off") == 0) {
     digitalWrite(LED_BUILTIN, HIGH);
   }
 }
 
-void setup() {
-  Serial.begin(9600);
-  connectToWifi();
-  checkForUpdate(true);
-  mqttClient.setServer(MQTT_BROKER, 1883);
-  mqttClient.setCallback(callback);
-
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-}
-
-void loop() {
-  checkForUpdate(false);
-  sendPing();
-
-  if (!mqttClient.connected()) {
-    reconnectMqttClient();
-  }
-  mqttClient.loop();
+void onMqttConnected() {
+  mqttHandler.subscribe("/foo/bar", onFooBar);
 }
